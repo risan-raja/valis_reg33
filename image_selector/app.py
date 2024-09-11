08937150ce1b44b224be4c4c65ea43d1b0089be7
Risan Raja <21f1000021@ds.study.iitm.ac.in>
2024-09-11 12:24:04 +0000

    676v

--- image_selector/app.py
+++ image_selector/app.py
@@ -1,6 +1,15 @@
 import glob
 import os
-from flask import Flask, Response, render_template, request, redirect, url_for, session, send_file
+from flask import (
+    Flask,
+    Response,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    session,
+    send_file,
+)
 from io import BytesIO
 from collections import defaultdict
 import json
@@ -8,73 +17,94 @@ import cv2
 from PIL import Image
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Needed for session management
+app.secret_key = "3829749812374"  # Needed for session management
 
 # Define the path where the slides (folders) are stored
 SLIDES_DIR = "/storage/valis_reg/stain_sep_results"
 slides = os.listdir(SLIDES_DIR)
 slides = sorted(slides)
 slide_images = {}
-
-if os.path.exists('selected_slide_images.json'):
-    with open('selected_slide_images.json', 'r') as f:
-        selected_slide_images = json.load(f)
-
 # Get the images for each slide
 for slide in slides:
     slide_path = os.path.join(SLIDES_DIR, slide)
     if os.path.isdir(slide_path):
-        slide_images[slide] = [img for img in os.listdir(slide_path) if img.endswith(('png', 'jpg', 'jpeg'))]
+        slide_images[slide] = [
+            img
+            for img in os.listdir(slide_path)
+            if img.endswith(("png", "jpg", "jpeg"))
+        ]
+
 
 selected_slide_images = defaultdict(list)
-for slide in slides:
-    selected_slide_images[slide] = []
-    for slide_img in slide_images[slide]:
-        if 'proc_mask' in slide_img:
-            if len(selected_slide_images[slide]) == 0:
-                selected_slide_images[slide].append(slide_img)
 
-with open('selected_slide_images.json', 'w') as f:
+
+if os.path.exists("selected_slide_images.json"):
+    with open("selected_slide_images.json", "r") as f:
+        selected_slide_images = json.load(f)
+
+else:
+    for slide in slides:
+        selected_slide_images[slide] = []
+        for slide_img in slide_images[slide]:
+            if "proc_mask" in slide_img:
+                if len(selected_slide_images[slide]) == 0:
+                    selected_slide_images[slide].append(slide_img)
+
+
+with open("selected_slide_images.json", "w") as f:
     json.dump(dict(selected_slide_images), f)
 
 
 # Route to display the slides and images
-@app.route('/')
+@app.route("/")
 def index():
     global selected_slide_images, slide_images, slides
     selected_slide_images
     # session['slides'] = slides
     # session['slide_images'] = slide_images
-    if 'processed_slides' not in session:
-        session['processed_slides'] = [False] * len(slides)  # False means unprocessed
-        session['current_slide'] = 0
+    if "processed_slides" not in session:
+        session["processed_slides"] = [False] * len(slides)  # False means unprocessed
+        session["current_slide"] = 0
         for slide_idx, slide in enumerate(slides):
             if slide in selected_slide_images:
                 if len(selected_slide_images[slide]) > 1:
-                    session['processed_slides'][slide_idx] = True
+                    session["processed_slides"][slide_idx] = True
                 else:
-                    session['processed_slides'][slide_idx] = False
+                    session["processed_slides"][slide_idx] = False
 
-    return render_template('grid.html', slides=slides, processed=session['processed_slides'])
+    return render_template(
+        "grid.html", slides=slides, processed=session["processed_slides"]
+    )
 
 
 
 # Route to display images from the selected slide folder
-@app.route('/slide/<int:slide_index>')
+@app.route("/slide/<int:slide_index>")
 def show_slide(slide_index):
     global slides, selected_slide_images, slide_images
     # slides = session.get('slides', [])
     current_slide = slides[slide_index]
     curr_slide_images = slide_images[current_slide]
-    main_images = [img for img in curr_slide_images if any(x in img for x in ['proc_mask', 'rf_mask', 'raw'])]
+    main_images = [
+        img
+        for img in curr_slide_images
+        if any(x in img for x in ["proc_mask", "rf_mask", "raw"])
+    ]
     stain_images = [img for img in curr_slide_images if img not in main_images]
     curr_selected_imgs = selected_slide_images[current_slide]
     # Update session to mark this slide as the current one
-    session['current_slide'] = slide_index
+    session["current_slide"] = slide_index
 
-    return render_template('slide.html', slide=current_slide, main_images=main_images, stain_images=stain_images, selected_files=curr_selected_imgs)
+    return render_template(
+        "slide.html",
+        slide=current_slide,
+        main_images=main_images,
+        stain_images=stain_images,
+        selected_files=curr_selected_imgs,
+    )
 
-@app.route('/image/<slide>/stain/<image_name>/<int:resolution>')
+
+@app.route("/image/<slide>/stain/<image_name>/<int:resolution>")
 def get_slide_stain_image(slide, image_name, resolution) -> Response:
     image_path = os.path.join(SLIDES_DIR, slide, image_name)
 
@@ -96,10 +126,11 @@ def get_slide_stain_image(slide, image_name, resolution) -> Response:
     pil_image.save(buffer, format="JPEG")
     buffer.seek(0)
 
-    return send_file(buffer, mimetype='image/jpeg')
+    return send_file(buffer, mimetype="image/jpeg")
 
-@app.route('/image/<slide>/main/<image_name>')
-def get_slide_main_image(slide, image_name)->Response:
+
+@app.route("/image/<slide>/main/<image_name>")
+def get_slide_main_image(slide, image_name) -> Response:
     image_path = os.path.join(SLIDES_DIR, slide, image_name)
 
     # Load the image using OpenCV
@@ -120,23 +151,37 @@ def get_slide_main_image(slide, image_name)->Response:
     pil_image.save(buffer, format="JPEG")
     buffer.seek(0)
 
-    return send_file(buffer, mimetype='image/jpeg')
+    return send_file(buffer, mimetype="image/jpeg")
 
-
-# Route to handle image selection
-@app.route('/select_images', methods=['POST'])
-def select_images():
-    global selected_slide_images, slides
+# Route to handle image selection for confirmation
+@app.route('/confirm_images', methods=['POST'])
+def confirm_images():
+    global slides
     selected_images = request.form.getlist('selected_images')
     current_slide_index = session['current_slide']
+    current_slide = slides[current_slide_index]
 
-    current_slide = slides[session['current_slide']]
+    # Render the confirmation page with selected images
+    return render_template('confirm.html', slide=current_slide, selected_images=selected_images)
+
+# Route to submit images after confirmation
+@app.route('/submit_images', methods=['POST'])
+def submit_images():
+    global slides, selected_slide_images
+    selected_images = request.form.getlist('selected_images')
+    current_slide_index = session['current_slide']
+    current_slide = slides[current_slide_index]
     selected_slide_images[current_slide] = selected_images
-    with open('selected_slide_images.json', 'w') as f:
+
+    with open("selected_slide_images.json", "w") as f:
         json.dump(dict(selected_slide_images), f)
     print("Selected Images:", selected_images)
-    # You can process the selected images here (store them in a database, move to a different folder, etc.)
+    # Store confirmed images with folder name
+    # session['selected_images'].extend([f"{current_slide}/{image}" for image in selected_images])
+
+    # Mark the slide as processed
     session['processed_slides'][current_slide_index] = True
+
     # Move to the next slide if available
     if current_slide_index + 1 < len(slides):
         session['current_slide'] = current_slide_index + 1
@@ -145,5 +190,28 @@ def select_images():
         # If no more slides, return to home
         return redirect(url_for('start'))
 
-if __name__ == '__main__':
+# Route to handle image selection
+@app.route("/select_images", methods=["POST"])
+def select_images():
+    global selected_slide_images, slides
+    selected_images = request.form.getlist("selected_images")
+    current_slide_index = session["current_slide"]
+
+    current_slide = slides[session["current_slide"]]
+    selected_slide_images[current_slide] = selected_images
+    with open("selected_slide_images.json", "w") as f:
+        json.dump(dict(selected_slide_images), f)
+    print("Selected Images:", selected_images)
+    # You can process the selected images here (store them in a database, move to a different folder, etc.)
+    session["processed_slides"][current_slide_index] = True
+    # Move to the next slide if available
+    if current_slide_index + 1 < len(slides):
+        session["current_slide"] = current_slide_index + 1
+        return redirect(url_for("show_slide", slide_index=session["current_slide"]))
+    else:
+        # If no more slides, return to home
+        return redirect(url_for("start"))
+
+
+if __name__ == "__main__":
     app.run(debug=True)
